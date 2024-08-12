7de7d35080d89e8eae50f267f4ba6fcde56b65b9
leemwangiw <lee.mwangi1@student.moringaschool.com>
2024-08-12 09:24:15 +0300

    commiting all

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,7 +2,7 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
+    const birthYear = 1986;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
     expect(ageOfPerson).toBe(38);
   });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
-// Your code here
+// src/utils.js
+export function currentAgeForBirthYear(birthYear) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+}
